973c3f51912c2e663909ffdba3f39418a78cf7e1
Kiran Anand <liteninkiran@hotmail.com>
2023-07-30 21:41:38 +0100

    012 - Adding rectangles to the SVG

--- src/app/charts/chart1/chart1.component.ts
+++ src/app/charts/chart1/chart1.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
     selector: 'app-chart1',
     templateUrl: './chart1.component.html',
     styleUrls: ['./chart1.component.scss'],
 })
-export class Chart1Component {
+export class Chart1Component implements OnInit {
+
+    public data = [125, 100, 50, 75, 200];
+
+    constructor() {
+
+    }
+
+    public ngOnInit(): void {
+
+    }
 
 }
