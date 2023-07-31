b0b88bcf10da6d9492bc71242352761ea0d003a3
Kiran Anand <liteninkiran@hotmail.com>
2023-07-31 17:26:06 +0100

    046 - Re-creating the chart using D3

--- src/app/charts/chart3/chart3.component.ts
+++ src/app/charts/chart3/chart3.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, ElementRef, Input, OnChanges, OnInit, SimpleChanges, ViewEncapsulation } from '@angular/core';
 import * as d3 from 'd3';
 
 @Component({
@@ -7,7 +7,9 @@ import * as d3 from 'd3';
     styleUrls: ['./chart3.component.scss'],
     encapsulation: ViewEncapsulation.None,
 })
-export class Chart3Component implements OnInit {
+export class Chart3Component implements OnInit, OnChanges {
+
+    @Input() public data: Array<number>;
 
     public host: any;
     public svg: any;
@@ -21,13 +23,20 @@ export class Chart3Component implements OnInit {
 
     public ngOnInit(): void {
         this.svg = this.host.select('svg');
+    }
+
+    public ngOnChanges(changes: SimpleChanges): void {
+        this.draw();
+    }
+
+    private draw() {
         this.svg
             .selectAll('rect')
-            .data([10, 20, 30])
+            .data(this.data || [])
             .enter()
             .append('rect')
             .attr('x', (d, i) => i * (this.rectWidth + this.padding))
             .attr('width', this.rectWidth)
-            .attr('height', (d) => d);
+            .attr('height', (d) => d.employee_age);
     }
 }
