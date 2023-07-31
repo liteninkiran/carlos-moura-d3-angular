d3935d44a1f04d22705c6b47d925e43db7e3e7d1
Kiran Anand <liteninkiran@hotmail.com>
2023-07-31 21:58:58 +0100

    050 - Adding margins to the chart

--- src/app/charts/chart3/chart3.component.ts
+++ src/app/charts/chart3/chart3.component.ts
@@ -16,6 +16,14 @@ export class Chart3Component implements OnInit, OnChanges {
     public dimensions: DOMRect;
     public rectWidth = 20;
     public padding = 3;
+    public innerWidth: number;
+    public innerHeight: number;
+    public margin = {
+        left: 10,
+        right: 20,
+        bottom: 16,
+        top: 15,
+    };
 
     public x = d3.scaleBand().paddingInner(0.2).paddingOuter(0.2);
     public y = d3.scaleLinear();
@@ -28,6 +36,8 @@ export class Chart3Component implements OnInit, OnChanges {
     public ngOnInit(): void {
         this.svg = this.host.select('svg');
         this.dimensions = this.svg.node().getBoundingClientRect();
+        this.innerWidth = this.dimensions.width - this.margin.left - this.margin.right;
+        this.innerHeight = this.dimensions.height - this.margin.top - this.margin.bottom;
     }
 
     public ngOnChanges(changes: SimpleChanges): void {
@@ -46,15 +56,15 @@ export class Chart3Component implements OnInit, OnChanges {
             .append('rect')
             .attr('x', (d) => this.x(d.id))
             .attr('width', this.x.bandwidth())
-            .attr('height', (d) => this.dimensions.height - this.y(d.employee_salary))
+            .attr('height', (d) => this.dimensions.height - this.margin.bottom - this.y(d.employee_salary))
             .attr('y', (d) => this.y(d.employee_salary));
     }
 
     private setParams(): void {
         const ids = this.data.map((d: any) => d.id);
-        this.x.domain(ids).range([0, this.dimensions.width]);
+        this.x.domain(ids).range([this.margin.left, this.innerWidth]);
         const maxSalary = Math.max(...this.data.map((item: any) => item.employee_salary)) * 1.2;
-        this.y.domain([0, maxSalary]).range([this.dimensions.height, 0]);
+        this.y.domain([0, maxSalary]).range([this.dimensions.height - this.margin.bottom, this.margin.top]);
     }
 
 }
