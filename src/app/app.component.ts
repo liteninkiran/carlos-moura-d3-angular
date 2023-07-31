b8e9b5741534b310336865fb0fab1068a9c0bebf
Kiran Anand <liteninkiran@hotmail.com>
2023-07-31 12:23:55 +0100

    031 - Receiving data as input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-    title = 'dashboard-one';
+export class AppComponent implements OnInit {
+    public title = 'Dashboard';
+    public data = [125, 100, 50, 75, 200, 60, 70];
+
+    public ngOnInit(): void {
+        setTimeout(() => {
+            console.log('updated');
+            this.data = [...this.data, 600];
+        }, 5000);
+    }
+
 }
--- src/app/charts/chart1/chart1.component.ts
+++ src/app/charts/chart1/chart1.component.ts
@@ -1,13 +1,14 @@
-import { Component, ElementRef, OnInit } from '@angular/core';
+import { Component, ElementRef, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 
 @Component({
     selector: 'app-chart1',
     templateUrl: './chart1.component.html',
     styleUrls: ['./chart1.component.scss'],
 })
-export class Chart1Component implements OnInit {
+export class Chart1Component implements OnInit, OnChanges {
 
-    public data = [125, 100, 50, 75, 200, 60, 70];
+    @Input() public data: Array<number>;
+
     public xLabels = ['A', 'B', 'C', 'D', 'E', 'F', 'G'];
     public rectWidth: number;
     public max: number;
@@ -19,7 +20,7 @@ export class Chart1Component implements OnInit {
     public left = 10;
     public right = 20;
     public bottom = 16;
-    public top = 55;
+    public top = 15;
     public innerWidth: number;
     public innerHeight: number;
 
@@ -32,11 +33,17 @@ export class Chart1Component implements OnInit {
         this.dimensions = svg.getBoundingClientRect();
         this.innerWidth = this.dimensions.width - this.left - this.right;
         this.innerHeight = this.dimensions.height - this.top - this.bottom;
+        this.setParams();
+    }
 
+    public ngOnChanges(changes: SimpleChanges): void {
+        this.setParams();
+    }
+
+    public setParams(): void {
         this.rectWidth = (this.innerWidth - 2 * this.outerPadding) / this.data.length;
         this.max = Math.max(...this.data) * 1.2;
         this.bandwidth = this.bandwidthCoef * this.rectWidth;
         this.padding = (1 - this.bandwidthCoef) * this.rectWidth;
     }
-
 }
