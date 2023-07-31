a3fa49f8426900512c56524defeb5235c93cf883
Kiran Anand <liteninkiran@hotmail.com>
2023-07-31 16:25:45 +0100

    037 - The SVG as a D3 selector

--- src/app/charts/chart3/chart3.component.ts
+++ src/app/charts/chart3/chart3.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef } from '@angular/core';
+import { Component, ElementRef, OnInit } from '@angular/core';
 import * as d3 from 'd3';
 
 @Component({
@@ -6,12 +6,16 @@ import * as d3 from 'd3';
     templateUrl: './chart3.component.html',
     styleUrls: ['./chart3.component.scss'],
 })
-export class Chart3Component {
+export class Chart3Component implements OnInit {
 
     public host: any;
+    public svg: any;
 
     constructor(elementRef: ElementRef) {
         this.host = d3.select(elementRef.nativeElement);
-        console.log(this);
+    }
+
+    public ngOnInit(): void {
+        this.svg = this.host.select('svg');
     }
 }
