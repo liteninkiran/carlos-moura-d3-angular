e7a9f36bff9abbe26c236df02a49b3678671b16c
Kiran Anand <liteninkiran@hotmail.com>
2023-08-01 18:52:52 +0100

    071 - Add the iris dataset to the app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,14 +10,17 @@ import { Observable } from 'rxjs';
 export class AppComponent implements OnInit {
     public title = 'Dashboard';
     public data1 = [125, 100, 50, 75, 200, 60, 70];
-    public $data2: Observable<any[]>;
+    public data2$: Observable<any[]>;
+    public iris$: Observable<any[]>;
 
     constructor(private api: ApiService) {
 
     }
 
     public ngOnInit(): void {
-        this.$data2 = this.api.getEmployees();
+        this.data2$ = this.api.getEmployees();
+        this.iris$ = this.api.getIris();
+        this.iris$.subscribe((c) => console.log(c));
         setTimeout(() => {
             this.data1 = [...this.data1, 600];
         }, 5000);
