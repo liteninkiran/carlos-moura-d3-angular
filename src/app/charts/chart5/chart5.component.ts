0be48171c5d194ab13997cfc8c496ca086674d69
Kiran Anand <liteninkiran@hotmail.com>
2023-08-02 00:40:08 +0100

    095 - Adding the methods

--- src/app/charts/chart5/chart5.component.ts
+++ src/app/charts/chart5/chart5.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, Input, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, ElementRef, Input, OnInit, ViewEncapsulation, OnChanges, SimpleChanges } from '@angular/core';
 import * as d3 from 'd3';
 
 @Component({
@@ -7,7 +7,7 @@ import * as d3 from 'd3';
     styleUrls: ['./chart5.component.scss'],
     encapsulation: ViewEncapsulation.None,
 })
-export class Chart5Component implements OnInit {
+export class Chart5Component implements OnInit, OnChanges {
 
     @Input() public data: any;
 
@@ -21,6 +21,46 @@ export class Chart5Component implements OnInit {
 
     public ngOnInit(): void {
         this.svg = this.host.select('svg');
+        this.setDimensions();
+        this.setElements();
+        this.updateChart();
         console.log(this);
+    }
+
+    public ngOnChanges(changes: SimpleChanges): void {
+        if (this.svg) {
+            this.updateChart();
+        }
+    }
+
+    private updateChart(): void {
+        this.setParams();
+        this.setLabels();
+        this.setAxis();
+        this.draw();
+    }
+
+    private setDimensions(): void {
+
+    }
+
+    private setElements(): void {
+
+    }
+
+    private setParams(): void {
+
+    }
+
+    private setLabels(): void {
+
+    }
+
+    private setAxis(): void {
+
+    }
+
+    private draw(): void {
+
     }
 }
