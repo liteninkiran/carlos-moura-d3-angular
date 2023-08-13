0591d5b5c074409898dbf79e8953507e36b3b44a
Kiran Anand <liteninkiran@hotmail.com>
2023-08-13 22:19:24 +0100

    292 - Finishing the setData() method

--- src/app/helpers/swarm.helper.ts
+++ src/app/helpers/swarm.helper.ts
@@ -1,4 +1,4 @@
-import { ISwarmData } from '../interfaces/chart.interfaces';
+import { ISwarmData, ISwarmDataElement } from '../interfaces/chart.interfaces';
 
 export class SwarmHelper {
     public data: ISwarmData = {
@@ -17,11 +17,28 @@ export class SwarmHelper {
         group: string,
         unit: string,
         decimals?: number
-    ) {
+    ): void {
+        let rounding: (value: number) => number;
+
+        if (decimals === undefined) {
+            rounding = (value: number) => value;
+        } else {
+            const factor = Math.pow(10, decimals);
+            rounding = (value: number) => Math.round(factor * value) / factor;
+        }
+
+        const elements: ISwarmDataElement[] = data.map((item: any) => ({
+            id: item[id],
+            label: item[label],
+            category: item[category],
+            group: item[group],
+            value: rounding(item[value]),
+        }));
+        
         this.data = {
             title,
             unit,
-            data: [],
+            data: elements,
         };
     }
 }
