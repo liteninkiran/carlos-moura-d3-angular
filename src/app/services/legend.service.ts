4b43dcd86a3b668b86a449e2e03f16cdb19db543
Kiran Anand <liteninkiran@hotmail.com>
2023-08-14 01:36:31 +0100

    320 - Legend services - adding properties and methods

--- src/app/services/legend.service.ts
+++ src/app/services/legend.service.ts
@@ -1,6 +1,35 @@
 import { Injectable } from '@angular/core';
+import ObjectHelper from '../helpers/object.helper';
 
 @Injectable()
-export abstract class LegendService {
+export abstract class LegendService<D, C> {
+    private _data: D = undefined as any;
+    private _config: C = undefined as any;
 
+    protected abstract defaultData: D;
+    protected abstract defaultConfig: C;
+
+    public set data(data: D) {
+        this._data = data;
+        this.onUpdateData();
+    }
+
+    public get data() {
+        return this._data || this.defaultData;
+    }
+
+    public set config(config: C) {
+        this._config = ObjectHelper.UpdateObjectWithPartialValues(this.defaultConfig, config);
+        this.onUpdateConfig();
+    }
+
+    public get config() {
+        return this._config || this.defaultConfig;
+    }
+
+    abstract onUpdateData: () => void;
+    abstract onUpdateConfig: () => void;
+
+    abstract generateItem: (selection: any) => void;
+    abstract updateItem: (selection: any) => void;
 }
--- src/app/services/list-legend.service.ts
+++ src/app/services/list-legend.service.ts
@@ -2,6 +2,25 @@ import { Injectable } from '@angular/core';
 import { LegendService } from './legend.service';
 
 @Injectable()
-export class ListLegendService extends LegendService {
+export class ListLegendService extends LegendService<any, any> {
+
+    protected override defaultData: any = { };
+    protected override defaultConfig: any = { };
+
+    public onUpdateData = (): void => {
+
+    }
+
+    public onUpdateConfig = (): void => {
+
+    }
+
+    public generateItem = (selection: any): void => {
+
+    }
+
+    public updateItem = (selection: any): void => {
+
+    }
 
 }
